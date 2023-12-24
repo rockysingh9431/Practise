0e6ecc1bb956db9a7d39f9991093f75f3f8d1691
rockysingh9431 <rockysingh943189@gmail.com>
2023-12-24 17:01:12 +0530

    Video 4 Done

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client";
+// const heading1 = React.createElement("h1", { id: "title",key:"2014" }, "heading1");
+// const heading2 = React.createElement("h1", { id: "title",key:"2015" }, "heading2");
+// const heading3 = React.createElement("h1", {id:"heading",key:"2016"}, "My name is rocky Singh Hello");
 
-const heading1 = React.createElement("h1", { id: "title" }, "heading1");
-const heading2 = React.createElement("h1", { id: "title" }, "heading2");
-const heading3 = React.createElement("h1", {id:"heading"}, "This is React");
+///Video 4
 
-const container = React.createElement("div", { id:"container" }, [
-  heading1,
-  heading2,heading3
-]);
+// JSX => React.createElement()=>Object=>HTML
+// Babel read JSX and convert it into readable code for JS Engine
+
+//This is JSX
+const heading = (
+  <div id="Container">
+    <h1 id="title">Heading 1</h1>
+    <h1 id="title2">Heading 2</h1>
+    <h1 id="title3">Heading 3</h1>
+  </div>
+);
+
+//components
+/*
+Functional Component
+Class Based Component
+*/
+
+// Functional Component
+
+const HeadingComponent= () => {
+  return (
+    <div id="Container">
+      {heading}
+      <h1 id="title">Heading 4</h1>
+      <h1 id="title2">Heading 5</h1>
+      <h1 id="title3">Heading 6</h1>
+    </div>
+  );
+};
+console.log("Rocky Singh");
+// const container = React.createElement("div", { id: "container" }, [
+//   heading1,
+//   heading2,
+//   heading3,
+// ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<HeadingComponent/>);//==root.render(HeadingComponent()) same
